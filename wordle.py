9b1afc47f15c2ef365f527f8169d89f60406be1d
bronny86 <bronnyhamilton@gmail.com>
2024-05-03 08:02:19 +1000

    added menu

--- wordle.py
+++ wordle.py
@@ -5,3 +5,6 @@ import nltk
 nltk.download('words')
 from nltk.corpus import words
 
+def print_menu():
+    print("Let's play Wordle!:")
+    print("Six chances to guess a 5 letter word. Type your first guess and hit enter!\n")
